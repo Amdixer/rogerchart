64a55a4035e4c1821140503159184a5b32b40ee0
Rogier Colijn <rogier@colijn.space>
2024-03-17 17:57:55 +0100

    Added comments to JS file.

--- RogerChart.js
+++ RogerChart.js
@@ -1,18 +1,10 @@
-
-var colours = ["#FF0000","#00FF00","#0000FF","#0F0F0F","#FF0000","#00FF00","#0000FF","#0F0F0F","#FF0000","#00FF00","#0000FF","#0F0F0F"]
-var i = 0;
-
 function createPlotDiv(){
     let div = document.createElement("div");
     document.getElementById("main").appendChild(div);
-
     div.style.width  = "100%";
     div.style.height = "100%";
     div.style.margin = '0';
     div.style.padding = '0';
-    // div.style.backgroundColor = colours[i];
-    // div.innerHTML = "TEST123";
-    i++;
     return div;
 
 }
@@ -30,45 +22,50 @@ function createPage(){
     //Determine how many rows and columns there are.
     var Rows = 0;
     var Cols = 0;
-    var templateCols    = "";
-    var templateRows    = "";
-
     Rows = page_shape.length;
     Cols = page_shape[0].length;
 
-    // for (row in page_shape){
-    //     for (col in page_shape[row]){
-    //         createPlotDiv();
-    //     }
-    // }
+    //Dependin gon the amount of rows and columns, setup the grid template columns
+    //parameters.
+    var templateCols    = "";
+    var templateRows    = "";
 
-    //Set up the grid.
+    //Iteratoe over the rows.
     for (var row = 0; row < Rows; row++){
         if(Rows === 1){
-            templateRows += "100% ";
+            templateRows += "100% ";        //If there's only 1 row, full height.
         }
         else{
-            templateRows += "50% ";
+            templateRows += "50% ";         //Else, cap it at 50%. TODO: Make this a setting in the future.
         }
     }
+
+    //Iterate over the columns, the width is 100/n_cols % per column.
     for (var col = 0; col < Cols; col++){
-        templateCols += String(Math.floor(100/Cols)) + "% ";
+        templateCols += String(100/Cols) + "% ";
     }
 
+    //Set up the grid.
     mainDiv.style.gridTemplateColumns = templateCols;
     mainDiv.style.gridTemplateRows = templateRows;
 
 
     for (p in raw_plot_options){
-        console.log(p);
+        //Parse the plot options and add to global list of options..
         var o    = JSON.parse(raw_plot_options[p]);
         plot_options.push(o);
 
+        //Initiate the chart. Adding a new div in the process.
         var chart = echarts.init(createPlotDiv());
+
+        //Set the options.
         chart.setOption(o); 
+
+        //Add the chart to the list of charts.
         charts.push(chart);
     }
 
+    //Bind the resize function to rescale all plots when the window is resized.
     window.addEventListener("resize", function () {
         for (c in charts){
             charts[c].resize();
@@ -76,4 +73,5 @@ function createPage(){
     });   
 }
 
+//Call function to create the page.
 createPage();
